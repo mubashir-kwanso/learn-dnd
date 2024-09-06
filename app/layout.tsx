3823fd29448e9b2fa59d742122ee03b7d65f9506
Mubashir Hassan <mhm13devk@gmail.com>
2024-09-06 15:13:54 +0500

    chore: setup shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { cn } from "@/utils/cn";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
 const inter = Inter({
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto p-4 text-center">
+    <main className="max-w-4xl mx-auto p-4 text-center space-y-4">
       <h1 className="text-4xl font-bold">Learn DND</h1>
-      <p>Learn DND is a project to learn drag and drop</p>
+      <p>Learn DND is a project to learn drag and drop!</p>
+      <Button variant={"default"}>Click me</Button>
     </main>
   );
 }
